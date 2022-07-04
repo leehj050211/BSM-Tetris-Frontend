f5c489c5ce1cc26250cf3658868d4a473cdd64d3
leehj050211 <leehj050211@gmail.com>
2022-07-04 21:47:13 +0900

    Add: 게임 조작키 설명, 로그인시 닉네임 입력 박스 숨김

--- src/components/game/title-screen.tsx
+++ src/components/game/title-screen.tsx
@@ -28,7 +28,7 @@ const TitleScreen: React.FC<PropsType> = (props: PropsType) => {
 
     const confirmUsername = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const username = inputRef.current?.value;
+        const username = user.isLogin? user.username: inputRef.current?.value;
         if (typeof username == 'undefined') {
             return alert('닉네임 설정 중에 문제가 발생하였습니다.');
         }
@@ -45,15 +45,28 @@ const TitleScreen: React.FC<PropsType> = (props: PropsType) => {
 
     return (
         <div className="title">
+            <ul className="game-help-box">
+                <li>Left: ←, Right: →</li>
+                <li>Soft Drop: ↓</li>
+                <li>Hard Drop: space</li>
+                <li>Rotate Left: z</li>
+                <li>Rotate Right: x</li>
+                <li>Hold: c</li>
+            </ul>
             <h1>BSM Tetris</h1>
+            <br />
             <form onSubmit={confirmUsername}>
-                <input
-                    ref={inputRef}
-                    name="username"
-                    type="text"
-                    placeholder=""
-                    required
-                />
+                {
+                    user.isLogin?
+                    null:
+                    (<input
+                        ref={inputRef}
+                        name="username"
+                        type="text"
+                        placeholder="닉네임 입력"
+                        required
+                    />)
+                }
                 <button>게임 시작</button>
             </form>
             <MyRanking user={user} setUser={setUser} />
